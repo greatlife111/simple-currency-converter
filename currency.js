94233c17f2c2fb58e2ca572ec8f2898937850e7f
lyq11 <springliu2003@gmail.com>
2023-08-26 20:10:18 -0700

    first

--- currency.js
+++ currency.js
@@ -27,4 +27,53 @@ document.addEventListener("DOMContentLoaded", function () {
         console.error("Error fetching data:", error);
       });
   });
+
+
+  // Now, add code to fetch historical exchange rate data and create a chart
+  const chartButton = document.getElementById("chart");
+  const rateChart = document.getElementById("rateChart");
+
+  chartButton.addEventListener("click", function() {
+    // Fetch historical exchange rate data
+    const apiKey2 = "LIWDVGA4KHF8MN8Z";
+    const historicalApiUrl = `https://wwwalphavantage.co/query?function=FX_DAILY&from_symbol=${fromCurrency}&to_symbol=${toCurrency}&apikey=${apiKey2}&outputsize=compact&datatype=json&from=${aWeekAgo.toISOString().split("T")[0]}&to=${today.toISOString().split("T")[0]}`;
+
+    const today = new Date();
+    const aWeekAgo = new Date();
+    aWeekAgo.setDate(today.getDate() - 7);
+
+
+    fetch(historicalApiUrl)
+      .then(response => response.json())
+      .then(data => {
+        // Extract dates and exchange rates from the fetched data
+        if (data["Time Series FX"]) {
+          const dates = Object.keys(data["Time Series FX"]);
+          const rates = dates.map(date => data["Time Series FX"][date]["1. open"]);
+  
+          
+          new Chart(rateChart, {
+            type: "line",
+            data: {
+              labels: dates,
+              datasets: [
+                {
+                  label: "Exchange Rate Trend",
+                  data: rates,
+                  borderColor: "blue",
+                  fill: false,
+                },
+              ],
+            },
+          });
+        } else {
+          console.error("Error fetching historical data:", error);
+        }
+      })
+      .catch(error => {
+        console.error("Error fetching historical data:", error);
+      });
+  });
+
+
 });
